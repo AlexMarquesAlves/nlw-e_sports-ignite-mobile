76a4e3d252483560ac95ec50f5aa81caacb85ede
¡Duque <atanaelleonardo@gmail.com>
2022-09-15 02:32:08 -0300

    🆕 FlatList Added

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -12,6 +12,20 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
     </View>
   );
 }
