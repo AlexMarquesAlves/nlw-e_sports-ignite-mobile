cefe20ec4494395e38be62163ffbed93e8efefab
¡Duque <atanaelleonardo@gmail.com>
2022-09-17 18:11:34 -0300

    🆕 Component { Game } Created

---
+++ src/screens/Game/index.tsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import { Text, View } from "react-native";
+
+export default class index extends PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>index</Text>
+      </View>
+    );
+  }
+}
